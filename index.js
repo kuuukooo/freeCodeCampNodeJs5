ab02d8a6fbce4c8808444f2676183bc5c75675e6
José Ramírez <jwh743@gmail.com>
2025-03-05 10:15:33 -0300

    Primer commit

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.get("/api/", function (req, res) {
+  res.send("Sube un archivo");
+});
 
-
+app.post("/api/fileanalyse", upload.single("uploaded_file"), (req, res) => {
+  console.log(req.file);
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
